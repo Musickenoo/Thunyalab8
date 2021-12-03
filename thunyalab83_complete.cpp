c7d97aad85f4b79b284776587a5e96038aade251
Thunyatorn Kongmoung <Thunyatorn1125@gmail.com>
2021-12-03 17:26:10 +0700

    add thunyalab83_complete.cpp

---
+++ thunyalab83_complete.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+char before(char x){
+    if(x == 'A'){
+        return  'Z' ;
+    }
+    
+    if('A'<x and x<='Z'){
+        return x-1;
+    }
+    else if('a'<x and x<'z'){
+        return '0';
+    }
+    else{ 
+        return '0' ;
+    }
+}
